8e1db8cc646a67ea56f7ebd87c143347401befcf
Binyamin Y Cohen <binyamincohen555@gmail.com>
2023-08-05 22:41:53 +0300

    Add typing overloads for string extension

--- src/extype/builtin_extensions/str_ext.py
+++ src/extype/builtin_extensions/str_ext.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Union
+from typing import TypeVar, Union, overload
 from ..extension_utils import extend_type_with, extension
 
 
@@ -18,6 +18,11 @@ class StrExtension:
     A class that contains methods to extend the builtin `str` type.
     """
 
+    @overload
+    def to_int(self: str, base: int = 10) -> int: ...
+    @overload
+    def to_int(self: str, base: int = 10, default: _T = ...) -> Union[int, _T]: ...
+
     @extension
     def to_int(self: str, base: int = 10, default: _T = _SENTINEL) -> Union[int, _T]:
         try:
@@ -26,6 +31,11 @@ class StrExtension:
             if default is _SENTINEL:
                 raise
             return default
+
+    @overload
+    def to_float(self: str) -> float: ...
+    @overload
+    def to_float(self: str, default: _T = ...) -> Union[float, _T]: ...
 
     @extension
     def to_float(self: str, default: _T = _SENTINEL) -> Union[float, _T]:
